c1add1f846333f3bfa60a8821a2cdb580e201bef
Esdras Caleb <esdrascaleb@gmail.com>
2024-09-04 10:29:43 -0300

    validation and branch

--- hook.php
+++ hook.php
@@ -6,14 +6,12 @@ $secret = $_ENV['SECRET'];
 
 // Get the payload from GitHub
 $payload = file_get_contents('php://input');
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE_256'] ?? '';
+$currentPath = __DIR__;
 
 // Verify the payload if the secret is set
 if ($secret) {
-    list($algo, $hash) = explode('=', $signature, 2) + ['', ''];
-    $payloadHash = hash_hmac('sha256', $payload, $secret);
-
-    if (!hash_equals($hash, $payloadHash)) {
+    $headers = getallheaders();
+    if (!hash_equals('sha256='.hash_hmac('sha256', $$payload, $secret), $headers['x-hub-signature-256'])) {
         header('HTTP/1.1 403 Forbidden');
         exit('Invalid signature');
     }
@@ -36,6 +34,7 @@ if($_ENV['TOKEN']&&$_ENV['TOKEN']){
 // Run the git pull command
 $output = [];
 $return_var = 0;
+
 if($_ENV['RESET_BEFORE_PULL']){
     exec('git reset --hard', $output, $return_var);
     if ($return_var === 0) {
@@ -45,11 +44,20 @@ if($_ENV['RESET_BEFORE_PULL']){
         echo 'Git reset failed';
     }
 }
+if($_ENV["BRANCH"]){
+    exec('git checkout '.$_ENV["BRANCH"], $output, $return_var);
+    if ($return_var === 0) {
+        echo 'Git checkout successful';
+    }
+    else {
+        echo 'Git checkout failed';
+    }
+}
 exec('git pull 2>&1', $output, $return_var);
 
 // Log the output for debugging (optional)
 if($_ENV['SAVE_LOG']){
-    file_put_contents('git-pull.log', implode(PHP_EOL, $output).PHP_EOL, FILE_APPEND);
+    file_put_contents("{$currentPath}/git-pull.log", implode(PHP_EOL, $output).PHP_EOL, FILE_APPEND);
 }
 
 // Send a response back to GitHub
